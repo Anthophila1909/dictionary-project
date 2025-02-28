2f137e3e01d245bcbd1f4ebe69572f2b8f9c8b46
Anthophila1909 <elena.w@hotmail.de>
2025-02-28 14:55:42 +0100

    Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    const apiKey = "93tdd4d3b4c2db3bcc87b00foc83ce4a";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
