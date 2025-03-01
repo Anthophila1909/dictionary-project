036ba7167747ff8d084f8f57ea12de4846e08c8b
Anthophila1909 <elena.w@hotmail.de>
2025-03-01 08:32:34 +0100

    Integrated Footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ export default function App() {
           <Dictionary />
         </main>
       </div>
+      <footer className="text-center">
+        This project was coded by Elena Würtz and is open-sourced on{" "}
+        <a
+          href="https://github.com/Anthophila1909/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://the-dictionary-application.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
